77b4047cca6141a3e30aab77b42bc83e76f65c3a
matchacookie <tongkhaiminh@gmail.com>
2022-07-19 21:31:33 +0700

    Reformat HTML and CSS and add more buttons

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const subtract = (number) => {};
 const multiplication = (number) => {};
 const division = (number) => {};
 // VARS
-const display = document.querySelector('.display');
-const numbers = document.querySelectorAll('.number');
-const operators = document.querySelectorAll('.operator');
+const numbers = document.querySelectorAll('[data-number]');
+const operators = document.querySelectorAll('[data-operator]');
+const equals = document.querySelector('[data-equals]');
+const allClear = document.querySelector('[data-ac]');
+const del = document.querySelector('[data-del]');
+const previous = document.querySelector('[data-prev]');
+const current = document.querySelector('[data-current]');

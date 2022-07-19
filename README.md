c226a63b6b7ac852017b7159ea43143a41031214
matchacookie <tongkhaiminh@gmail.com>
2022-07-19 18:39:38 +0700

    Create HTML and CSS

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # calculator-top
+CALCULATOR FOR THE ODIN PROJECT
---
+++ script.js
@@ -0,0 +1,8 @@
+const add = (number) => {};
+const subtract = (number) => {};
+const multiplication = (number) => {};
+const division = (number) => {};
+// VARS
+const display = document.querySelector('.display');
+const numbers = document.querySelectorAll('.number');
+const operators = document.querySelectorAll('.operator');
